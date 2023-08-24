af82fdeb8856c0e9ebd98af3a8ef4c696e30ba85
FrancescoGnerre <francesco.gnerre@gmail.com>
2023-08-24 20:15:35 +0000

    Buttons Work Basic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from flask.helpers import url_for
 import json
 import jsonify
 
@@ -31,9 +32,11 @@ p_List = [{
 }]
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def main_screen():
-  return render_template('home.html', players=p_List)
+  if request.method=="GET":
+    return render_template('home.html', players=p_List)
+  
 
 
 @app.route("/api/players")
@@ -41,9 +44,10 @@ def list_players():
   return jsonify(p_List)
 
 
-@app.route("/settings")
+@app.route("/settings", methods=["GET", "POST"])
 def settings_screen():
-  return render_template('settings.html')
+  if request.method=="GET":
+    return render_template('settings.html')
 
 
 if __name__ == "__main__":
---
+++ static/script.js
@@ -0,0 +1,21 @@
+function toHome(){
+  window.location.href = "https://factionizer.bovineghoti.repl.co/";
+}
+function toSettings(){
+  window.location.href = "https://factionizer.bovineghoti.repl.co/settings";
+}
+function addPlayer() {
+  alert("Player Name");
+}
+function confirmSettings(){
+  alert("Confirm these settigns");
+}
+function clearPlayersAll() {
+  alert("TBD");
+}
+function clearPlayer(p_name) {
+  alert(p_name);
+}
+function startGame() {
+  alert("TBD");
+}
