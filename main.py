8ab2dba99199753c033fdb37e934e7f05a294cfb
FrancescoGnerre <francesco.gnerre@gmail.com>
2023-09-05 03:11:52 +0000

    clean up main.py

--- main.py
+++ main.py
@@ -6,32 +6,6 @@ import jsonify
 app = Flask(__name__)
 p_Open = open('jxt//players.json')
 p_List = json.load(p_Open)
-#p_List = json.loads(p_temp)
-"""
-p_List = [{
-  "p_name": "Francesco",
-  "p_faction_a": "A",
-  "p_faction_b": "B",
-  "p_faction_c": "C",
-  "p_blue_1": "U1",
-  "p_blue_2": "U2",
-  "p_blue_3": "U3",
-  "p_red_1": "R1",
-  "p_red_2": "R2"
-}, {
-  "p_name": "Melanie",
-  "p_faction_a": "A1",
-  "p_faction_b": "B1",
-  "p_faction_c": "C1",
-  "p_blue_1": "U11",
-  "p_blue_2": "U21",
-  "p_blue_3": "U31",
-  "p_red_1": "R11",
-  "p_red_2": "R21"
-}, {
-  "p_name": "Luciano"
-}]
-"""
 
 
 @app.route("/", methods=["GET", "POST"])
